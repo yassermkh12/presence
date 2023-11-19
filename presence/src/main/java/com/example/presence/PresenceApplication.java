f05cf31875a75a895706e6c5589ba9f47ecb117c
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-19 14:36:40 +0100

    Add files via upload

---
+++ presence/src/main/java/com/example/presence/PresenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.presence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresenceApplication.class, args);
+	}
+
+}
