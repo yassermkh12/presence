6b578deae0b0a427144b2d3a4c0ca94d8f9446df
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-19 20:18:09 +0100

    20:18 crud etudiant

--- presence/src/main/java/com/example/presence/controllers/EtudiantController.java
+++ presence/src/main/java/com/example/presence/controllers/EtudiantController.java
@@ -1,4 +1,27 @@
 package com.example.presence.controllers;
 
+import com.example.presence.entities.Etudiant;
+import com.example.presence.entitiesDto.EtudiantDto;
+import com.example.presence.services.EtudiantService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
 public class EtudiantController {
+    @Autowired
+    private EtudiantService etudiantService;
+    @GetMapping("/all")
+    public ResponseEntity<List<EtudiantDto>> getAllEtudiant(){
+        List<EtudiantDto> etudiantDtos = etudiantService.getAllEtudiant();
+        return new ResponseEntity<>(etudiantDtos, HttpStatus.OK);
+    }
 }
--- presence/src/main/java/com/example/presence/services/EtudiantService.java
+++ presence/src/main/java/com/example/presence/services/EtudiantService.java
@@ -1,16 +1,34 @@
 package com.example.presence.services;
 
+import com.example.presence.entities.Etudiant;
 import com.example.presence.entitiesDto.EtudiantDto;
 import com.example.presence.repositories.IEtudiantRepository;
+import com.example.presence.transformers.EtudiantTransformer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
+import java.util.*;
+
 
 @Service
 public class EtudiantService {
     @Autowired
     private IEtudiantRepository etudiantRepository;
     //getAll
-
+    public List<EtudiantDto> getAllEtudiant(){
+        List<Etudiant> etudiants = etudiantRepository.findAll();
+        return EtudiantTransformer.entityToDtoList(etudiants);
+    }
+    public Optional<EtudiantDto> getEtudiantById(Long id){
+        Optional<Etudiant> etudiant = etudiantRepository.findById(id);
+        return Optional.ofNullable(etudiant.map(EtudiantTransformer::entityToDto).orElse(null));
+    }
+    public EtudiantDto saveEtudiant(EtudiantDto etudiantDto){
+        Etudiant etudiant = EtudiantTransformer.dtoToEntity(etudiantDto);
+        etudiantRepository.save(etudiant);
+        return EtudiantTransformer.entityToDto(etudiant);
+    }
+    public void deleteEtudiant(Long id){
+        etudiantRepository.deleteById(id);
+    }
 }
