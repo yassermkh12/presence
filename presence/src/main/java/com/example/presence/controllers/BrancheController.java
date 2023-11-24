7558043a851e05d3a0d90f1a8ac5539051ef3b48
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-24 17:13:03 +0100

    17:13 Branche crud

--- presence/src/main/java/com/example/presence/controllers/BrancheController.java
+++ presence/src/main/java/com/example/presence/controllers/BrancheController.java
@@ -1,15 +1,15 @@
 package com.example.presence.controllers;
 
 import com.example.presence.entitiesDto.BrancheDto;
+import com.example.presence.exceptions.NotFoundException;
 import com.example.presence.services.IBrancheService;
 import com.example.presence.services.impl.BrancheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 @RestController
@@ -22,5 +22,24 @@ public class BrancheController {
         List<BrancheDto> brancheDtos = brancheService.getAllBranche();
         return new ResponseEntity<>(brancheDtos, HttpStatus.OK);
     }
-
+    @GetMapping("/by-id/{id}")
+    public ResponseEntity<BrancheDto> getBrancheById(@PathVariable Long id) throws NotFoundException {
+        BrancheDto brancheDto = brancheService.getBrancheById(id);
+        return new ResponseEntity<>(brancheDto, HttpStatus.OK);
+    }
+    @PostMapping("/save")
+    public ResponseEntity<BrancheDto> saveBranche(@RequestBody BrancheDto brancheDto){
+        BrancheDto createBrancheDto = brancheService.saveBranche(brancheDto);
+        return new ResponseEntity<>(createBrancheDto, HttpStatus.CREATED);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<BrancheDto> updateBranche(@PathVariable Long id, @RequestBody BrancheDto brancheDto) throws NotFoundException {
+        BrancheDto updateBrancheDto = brancheService.updateBranche(id,brancheDto);
+        return new ResponseEntity<>(updateBrancheDto, HttpStatus.OK);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteBranche(@PathVariable Long id){
+        brancheService.deleteBranche(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
