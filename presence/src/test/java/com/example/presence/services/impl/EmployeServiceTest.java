5391a6817e36d7044ad09714d3e8d3da2543c7ab
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-12-06 11:53:42 +0100

    11:53 test service employe

--- presence/src/test/java/com/example/presence/services/impl/EmployeServiceTest.java
+++ presence/src/test/java/com/example/presence/services/impl/EmployeServiceTest.java
@@ -1,11 +1,14 @@
 package com.example.presence.services.impl;
 
+import com.example.presence.PresenceApplication;
 import com.example.presence.entities.Employe;
 import com.example.presence.entitiesDto.EmployeDto;
+import com.example.presence.exceptions.NotFoundException;
 import com.example.presence.repositories.IEmployeRepository;
 import com.example.presence.services.IEmployeService;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -16,14 +19,14 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-
+//@RunWith(EmployeServiceTest.class)
 @SpringBootTest
+final
 class EmployeServiceTest {
     @Mock
     private IEmployeRepository employeRepository;
     @InjectMocks
-    private IEmployeService employeService;
-
+    private EmployeService employeService;
     private Employe employe;
     @BeforeEach
     public void setUp(){
@@ -42,6 +45,25 @@ class EmployeServiceTest {
         List<EmployeDto> employeDtos = employeService.getAllEmploye();
 
         assertEquals(1,employeDtos.size());
+        assertEquals(employe.getId(),employeDtos.get(0).getId());
+        assertEquals(employe.getNom(),employeDtos.get(0).getNom());
+        assertEquals(employe.getPrenom(),employeDtos.get(0).getPrenom());
+        assertEquals(employe.getEmail(),employeDtos.get(0).getEmail());
+        assertEquals(employe.getPoste(),employeDtos.get(0).getPoste());
+        assertEquals(employe.getNumeroTelephone(),employeDtos.get(0).getNumeroTelephone());
+    }
+    @Test
+    public void testGetById() throws NotFoundException {
+        when(employeRepository.findById(1L))
+                .thenReturn(Optional.of(employe));
 
+        EmployeDto employeDto = employeService.getEmployeById(1L);
+
+        assertEquals(employe.getId(),employeDto.getId());
+        assertEquals(employe.getNom(),employeDto.getNom());
+        assertEquals(employe.getPrenom(),employeDto.getPrenom());
+        assertEquals(employe.getEmail(),employeDto.getEmail());
+        assertEquals(employe.getPoste(),employeDto.getPoste());
+        assertEquals(employe.getNumeroTelephone(),employeDto.getNumeroTelephone());
     }
 }
