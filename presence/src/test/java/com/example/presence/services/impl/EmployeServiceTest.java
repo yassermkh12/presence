f0dc08256e2925cbc12c0e8fcf661facc5189ea1
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-12-06 10:57:26 +0100

    10:57 test service employe

--- presence/src/test/java/com/example/presence/services/impl/EmployeServiceTest.java
+++ presence/src/test/java/com/example/presence/services/impl/EmployeServiceTest.java
@@ -1,29 +1,47 @@
 package com.example.presence.services.impl;
 
+import com.example.presence.entities.Employe;
 import com.example.presence.entitiesDto.EmployeDto;
 import com.example.presence.repositories.IEmployeRepository;
 import com.example.presence.services.IEmployeService;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 class EmployeServiceTest {
     @Mock
     private IEmployeRepository employeRepository;
     @InjectMocks
     private IEmployeService employeService;
+
+    private Employe employe;
+    @BeforeEach
+    public void setUp(){
+        employe = new Employe();
+        employe.setId(1L);
+        employe.setNom("mokhtari");
+        employe.setPrenom("yassir");
+        employe.setPoste("pes");
+        employe.setNumeroTelephone("0654421314");
+        employe.setEmail("yassir@gmail.com");
+    }
     @Test
-    public void testSaveEmploye(){
-        EmployeDto employeDto = new EmployeDto();
-        employeDto.setNom("mokhtari");
-        employeDto.setPrenom("yasser");
-        employeDto.setEmail("yasser.mokhtari@gmail.com");
-        employeDto.setNumeroTelephone("0654421314");
-        employeDto.setPoste("java");
+    public void testGetAllEmploye(){
+        when(employeRepository.findAll()).thenReturn(Arrays.asList(employe));
+
+        List<EmployeDto> employeDtos = employeService.getAllEmploye();
+
+        assertEquals(1,employeDtos.size());
 
     }
 }
